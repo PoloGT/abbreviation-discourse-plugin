f9db80fe7180ef4187a568daa1d40e595197fdeb
alposer <alposer@gmail.com>
2016-01-22 21:11:19 +0100

    modified plugin.rb to show url

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,7 @@
 # about: Plugin to substitute abbreviations with a title with the full word
 # version: 0.0.1
 # authors: Alfonso Polo
+# url: https://github.com/discourse/discourse-tagging
 
 enabled_site_setting :abbreviations_plugin_enabled
 
