3fdfa137f2960c6d26f835710fac1f73345fd6cf
alposer <alposer@gmail.com>
2016-01-22 09:19:45 +0100

    First commit

---
+++ plugin.rb
@@ -0,0 +1,8 @@
+# name: abbreviations
+# about: Plugin to substitute abbreviations with a title with the full word
+# version: 0.0.1
+# authors: Alfonso Polo
+
+enabled_site_setting :abbreviations_plugin_enabled
+
+register_asset "javascripts/abbreviations_parse.js", :server_side
