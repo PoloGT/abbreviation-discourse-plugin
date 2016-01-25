66a61b8cfa9b21c8671ce9ecb92ebea636f2c100
alposer <alposer@gmail.com>
2016-01-25 20:23:24 +0100

    Suit settings to a common example

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 # about: Plugin to substitute abbreviations with a title with the full word
 # version: 0.0.1
 # authors: Alfonso Polo
-# url: https://github.com/discourse/discourse-tagging
+# url: https://github.com/PoloGT/abbreviation-discourse-plugin.git 
 
 enabled_site_setting :abbreviations_plugin_enabled
 
