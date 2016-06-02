c98eaa7428d75f2e81786306ded2b2f795949ddc
Guo Xiang Tan <tgx_world@hotmail.com>
2016-06-02 11:41:01 +0800

    Use dialect preprocessor that calls a preprocessing function.

--- plugin.rb
+++ plugin.rb
@@ -2,9 +2,10 @@
 # about: Plugin to substitute abbreviations with a title with the full word
 # version: 0.0.1
 # authors: Alfonso Polo
-# url: https://github.com/PoloGT/abbreviation-discourse-plugin.git 
+# url: https://github.com/PoloGT/abbreviation-discourse-plugin.git
 
 enabled_site_setting :abbreviations_plugin_enabled
 
-register_asset "javascripts/abbreviations_parse.js", :server_side
+register_asset "javascripts/abbreviations_dialect.js", :server_side
+register_asset "javascripts/lib/abbreviations.js", :server_side
 register_asset "stylesheets/abbreviation_plugin.css"
