657ecd619f4a1f358c86aa2abc6a1ec97a1f837d
alposer <alposer@gmail.com>
2016-08-10 12:40:21 +0200

    FIX backwards compatibility

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,10 @@
 # name: abbreviations
 # about: Plugin to substitute abbreviations with a title with the full word
-# version: 0.0.1
+# version: 0.0.3
 # authors: Alfonso Polo
 # url: https://github.com/PoloGT/abbreviation-discourse-plugin.git
 
 enabled_site_setting :abbreviations_plugin_enabled
 
+register_asset "javascripts/abbreviations_dialect.js", :server_side
 register_asset "stylesheets/abbreviation_plugin.css"
